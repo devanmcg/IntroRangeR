39c99bb37c3a294523ea76ddcf87d03217a4be5b
devanmcg <devan.mcgranahan@gmail.com>
2020-02-29 15:54:20 -0600

    update stats script

--- 08_BasicStatistics/BasicStats.R
+++ 08_BasicStatistics/BasicStats.R
@@ -1,51 +1,56 @@
-pacman::p_load(plyr, ggplot2, GGally) 
+# An Introduction to R
+# Devan Allen McGranahan (devan.mcgranahan@gmail.com)
+# 
+# Lesson 8: Basic statistical analysis
+#
+  if (!require("pacman")) install.packages("pacman")
+  pacman::p_load(tidyverse, GGally) 
 
-str(mtcars)       # A more complex dataset
-
-# Changing component classes with as. functions
-
-mtcars$cyl <- as.factor(mtcars$cyl)
-mtcars$vs <- as.factor(mtcars$vs)
-mtcars$gear <- as.factor(mtcars$gear)
-mtcars$carb <- as.factor(mtcars$carb)
-mtcars$am <- revalue(as.factor(mtcars$am), c("0"="Automatic", 
-                                             "1"="Manual"))
+  mtcars <- mutate_at(mtcars, vars(cyl, vs, am,
+                                   gear, carb), 
+                      as.character ) %>%
+              mutate(am = recode(am, "0"="Automatic",   
+                                     "1"="Manual")) %>% 
+                rename(transmission = am) 
 
 # Basic pairwise comparisons
 
 # Do manual transmissions really get better fuel economy?
-  ggplot(mtcars, aes(x=am, y=mpg)) + theme_bw() +
-    geom_boxplot(aes(fill=am)) 
+  ggplot(mtcars, aes(x=transmission, y=mpg)) + theme_bw(14) +
+    geom_boxplot(aes(fill=transmission), 
+                 size = 1.5, show.legend =F) 
 
 # Check distribution
-  ggplot(data=mtcars, aes(x=mpg)) + theme_bw() + 
+  ggplot(mtcars, aes(x=mpg)) + theme_bw(14)  +
+    geom_density(alpha=.5, fill="lightgreen") + 
     geom_histogram(aes(y=..density..),      
-                   binwidth=0.1, fill="purple", alpha=0.8, colour="black") +
-    geom_density(aes(fill=mpg), alpha=.5, fill="lightgreen")
+                   binwidth=1, fill="purple", 
+                   alpha=0.8, colour="black")
   
-  ggplot(data=mtcars, aes(x=log(mpg))) + theme_bw() + 
+  ggplot(mtcars, aes(x=log(mpg))) + theme_bw(14) +
+    geom_density(alpha=.5, fill="lightgreen") + 
     geom_histogram(aes(y=..density..),      
-                   binwidth=0.1, fill="purple", alpha=0.8, colour="black") +
-    geom_density(aes(fill=mpg), alpha=.5)
+                   binwidth=0.1, fill="purple", 
+                   alpha=0.8, colour="black") 
   
-  ggplot(data=mtcars, aes(x=log(mpg))) + theme_bw() + 
-    geom_histogram(aes(y=..density.., fill=am),      
-                   binwidth=0.1,  alpha=0.8, colour="black") +
-    geom_density(aes(fill=am), alpha=.5)
+  ggplot(mtcars, aes(x=log(mpg), fill=transmission)) + theme_bw(14) + 
+    geom_histogram(aes(y=..density..),      
+                   binwidth=0.1,  alpha=0.8, colour="black")  +
+    geom_density(alpha=.5) 
 
 # T test 
 
-  t.test(mpg ~ am, mtcars)
+  t.test(mpg ~ transmission, mtcars)
   
 # ANOVA 
-  am.lm <- lm(mpg ~ am, mtcars)
-  am.lm
-  summary(am.lm)
-  anova(am.lm)
+  tr.lm <- lm(mpg ~ transmission, mtcars)
+  tr.lm
+  summary(tr.lm)
+  anova(tr.lm)
  
 # Linear regression 
 
-  sp.gg <- ggplot(mtcars, aes(x=hp, y=mpg)) + theme_bw() 
+  sp.gg <- ggplot(mtcars, aes(x=hp, y=mpg)) + theme_bw(14) 
   sp.gg + geom_point()
   
   sp.gg + geom_point() +
@@ -58,8 +63,8 @@ mtcars$am <- revalue(as.factor(mtcars$am), c("0"="Automatic",
 # Multiple comparisons 
   # Factors with >2 levels 
   
-  ggplot(mtcars, aes(x=cyl, y=mpg)) + theme_bw() +
-    geom_boxplot(aes(fill=cyl)) 
+  ggplot(mtcars, aes(x=cyl, y=mpg)) + theme_bw(14) +
+    geom_boxplot(aes(fill=cyl), show.legend = F) 
   
   cyl.lm <- lm(mpg ~ cyl, mtcars)
   summary(cyl.lm)
@@ -68,36 +73,37 @@ mtcars$am <- revalue(as.factor(mtcars$am), c("0"="Automatic",
   
 # Multiple regression 
   # Two continuous variables 
-    ggpairs(mtcars, columns = c("hp","mpg", "wt"))
+  GGally::ggpairs(mtcars, columns = c("hp","mpg", "wt"))
     
     mrc.lm <- lm(mpg ~ hp + wt, mtcars)
     summary(mrc.lm)
     anova(mrc.lm)
   
   # Simple model comparison 
-    m1 <- lm(mpg ~ 1, mtcars)
-    m2 <- lm(mpg ~ hp, mtcars)
-    m3 <- lm(mpg ~ hp + wt, mtcars)
-    anova(m1, m2, m3)
-    coef(m3) # View regression coefficients (similar to effect size)
-    confint(m3)
+    m0 <- lm(mpg ~ 1, mtcars)
+    m1 <- lm(mpg ~ hp, mtcars)
+    m2 <- lm(mpg ~ hp + wt, mtcars)
+    anova(m0, m1, m2)
+    coef(m2) # View regression coefficients (similar to effect size)
+    confint(m2)
 
   # Continuous + categorical variables
-    mrd.gg <- ggplot(mtcars, aes(x=hp, y=mpg, color=am)) + theme_bw() 
+    mrd.gg <- ggplot(mtcars, aes(x=hp, y=mpg, 
+                                 color=transmission)) + theme_bw(14) 
     mrd.gg + geom_point() 
     mrd.gg + geom_point() +
             geom_smooth(method="lm", se=FALSE)
     
-    mrd.lm <- lm(mpg ~ hp + am, mtcars)
+    mrd.lm <- lm(mpg ~ hp + transmission, mtcars)
     summary(mrd.lm)
     anova(mrd.lm)
   
-    m1 <- lm(mpg ~ 1, mtcars)
-    m2 <- lm(mpg ~ hp, mtcars)
-    m3 <- lm(mpg ~ hp + am, mtcars)
-    anova(m1, m2, m3)
-    coef(m3) 
-    confint(m3)
+    m0 <- lm(mpg ~ 1, mtcars)
+    m1 <- lm(mpg ~ hp, mtcars)
+    m2 <- lm(mpg ~ hp + transmission, mtcars)
+    anova(m0, m1, m2)
+    coef(m2) 
+    confint(m2)
     
   # Testing for statistical interactions 
      int.lm1 <- lm(mpg ~ hp:wt, mtcars)
@@ -107,8 +113,3 @@ mtcars$am <- revalue(as.factor(mtcars$am), c("0"="Automatic",
      anova(int.lm1)
      anova(int.lm2)
      anova(int.lm3)
-  
-
-
-
-  

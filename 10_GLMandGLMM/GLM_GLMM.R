42be27e635e4ff8d1d1b73041b111789d628f1c5
devanmcg <devan.mcgranahan@gmail.com>
2020-03-30 11:28:39 -0500

    update class script

--- 10_GLMandGLMM/GLM_GLMM.R
+++ 10_GLMandGLMM/GLM_GLMM.R
@@ -1,39 +1,65 @@
-pacman::p_load(plyr, dplyr,  ggplot2, lme4, car)
+# An Introduction to R
+# Devan Allen McGranahan (devan.mcgranahan@gmail.com)
+# 
+# Lesson 10: Fitting GLM and GLMM regression models
+#
+# Packages
+  # Need to have these installed, but recommend *not* loading them
+    install.packages(c("car", "lme4"))
 
-clev.d <- read.csv("./course materials/class session materials/data/AllClevelandCrimeData.csv")
-
-str(clev.d)
-
+  # Always need pacman and tidyverse!
+    if (!require("pacman")) install.packages("pacman")
+    pacman::p_load(tidyverse) 
+  
+# Load data 
+  clev.d <- read_csv("./data/AllClevelandCrimeData.csv")
+  clev.d 
+  
+  clev.d <- clev.d %>% mutate(GameDay = recode(GameDay, "0" = "No", 
+                                                        "1" = "Yes") ) 
+  
 # Count data 
 
-# Are there more charges on game days?
-  # Plot
-  ggplot(clev.d) + 
-    geom_bar(aes(x=factor(GameDay)), stat="count")
+# Are there more charges on game days? 
+  ggplot(clev.d) + theme_bw(16) +
+    geom_bar(aes(x=GameDay), stat="count")
   
-  # Test
-  GD <- ddply(clev.d, .(GameDay),
-                      summarize, 
-              charges=length(ChargeType))
+  # Summarize
+  GD <- clev.d %>%
+          group_by(GameDay) %>%
+            summarize(charges=n())
   GD
 
-  null.t <- glm(charges ~ 1, GD, family=poisson(link = "log"))
-  gd <- glm(charges ~ GameDay, GD, family=poisson(link = "log"))
-  anova(null.t, gd)
-  car::Anova(gd)
-  confint(gd, level = 0.95)
+  # Test 
+    # fit the GLM model 
+      glm_gd <- glm(charges ~ GameDay, GD, family=poisson(link = "log"))
+    
+    # Evaluate
+     summary(glm_gd)  # Pretty familiar    
+     anova(glm_gd)    # Hey, what gives?  
+     anova(glm_gd, test = "Chisq") # That's more like it 
+     car::Anova(glm_gd)   # Different but same 
+    
 
 # Non-independence 
-  GD.v <- ddply(clev.d, .(Venue, GameDay),
-              summarize, 
-              charges=length(ChargeType))
+    
+ ggplot(clev.d) + theme_bw(16) +
+   geom_bar(aes(x=GameDay, fill=Venue), 
+            stat="count",
+            position = position_dodge2())
+ 
+  GD.v <- clev.d %>%
+            group_by(Venue, GameDay) %>%
+              summarize(charges=n()) %>% ungroup
   GD.v
 
-  null.t <- glmer(charges ~ 1 + (1|Venue), GD.v, 
-                  family=poisson(link = "log"))
-  gd <- glmer(charges ~ GameDay + (1|Venue), GD.v, 
-              family=poisson(link = "log"))
-  anova(null.t, gd)
-  Anova(gd)
-  confint(gd, level = 0.95)  
+  glmer_gd <- lme4::glmer(charges ~ GameDay + (1|Venue), GD.v, 
+                          family=poisson(link = "log"))
+  summary(glmer_gd)    
+  anova(glmer_gd, test = "Chisq") # Trick doesn't work on glmer
+  car::Anova(glmer_gd)  # This still works  
 
+  # TO use anova(), create a null model and compare
+  glmer_0 <- lme4::glmer(charges ~ 1 + (1|Venue), GD.v, 
+                          family=poisson(link = "log"))
+  anova(glmer_0, glmer_gd) 
